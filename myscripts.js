105398ed8c28233185b04c0d036caa4f64650e40
Vail Algatt <vail.algatt@gmail.com>
2017-04-20 13:28:43 -0700

    first commit for cat-lady project

---
+++ myscripts.js
@@ -0,0 +1,32 @@
+var petCount = document.getElementById("petCount")
+var catStatus = document.getElementById("catStatus")
+var counter = 0
+var catButton = document.getElementById("catButton")
+
+
+
+function petCat(){
+    counter++
+    petCount.innerHTML = counter;
+    if(counter > 5){
+        catStatus.innerHTML="rawwr"
+    } 
+    if(counter == 9){
+       catStatus.innerHTML="Scratch"
+    }
+    if(counter == 10){
+        catStatus.innerHTML="Cat ran away"
+        catButton.disabled = true;
+        catPic.src=""
+    }
+}
+
+function reset(){
+    petCount.innerHTML = counter
+    catStatus.innerHTML = "Purrrr"
+    catButton.disabled = false
+    catPic.src="Cat-png.png"
+}
+
+
+
